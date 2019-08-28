d1be638757372d79876b35ef2e7c77b2079bf8ec
Daniel Cifuentes <danixbox1313@gmail.com>
2019-08-27 21:45:40 -0500

    invariant holds

--- src/main/java/edu/eci/arsw/highlandersim/Immortal.java
+++ src/main/java/edu/eci/arsw/highlandersim/Immortal.java
@@ -2,105 +2,105 @@ package edu.eci.arsw.highlandersim;
 
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Immortal extends Thread {
-	
+
 	private boolean paused;
 
-    private ImmortalUpdateReportCallback updateCallback=null;
-    
-    private int health;
-    
-    private int defaultDamageValue;
+	private ImmortalUpdateReportCallback updateCallback = null;
 
-    private final List<Immortal> immortalsPopulation;
+	private AtomicInteger health;
 
-    private final String name;
+	private int defaultDamageValue;
 
-    private final Random r = new Random(System.currentTimeMillis());
+	private final List<Immortal> immortalsPopulation;
 
+	private final String name;
 
-    public Immortal(String name, List<Immortal> immortalsPopulation, int health, int defaultDamageValue, ImmortalUpdateReportCallback ucb) {
-        super(name);
-        this.updateCallback=ucb;
-        this.name = name;
-        this.immortalsPopulation = immortalsPopulation;
-        this.health = health;
-        this.defaultDamageValue=defaultDamageValue;
-    }
+	private final Random r = new Random(System.currentTimeMillis());
 
-    public void run() {
+	public Immortal(String name, List<Immortal> immortalsPopulation, int health, int defaultDamageValue,
+			ImmortalUpdateReportCallback ucb) {
+		super(name);
+		this.updateCallback = ucb;
+		this.name = name;
+		this.immortalsPopulation = immortalsPopulation;
+		this.health = new AtomicInteger(health);
+		this.defaultDamageValue = defaultDamageValue;
+	}
 
-        while (true) {
-        	
-        	synchronized(immortalsPopulation) {
-        		while(paused) {
-        			try {
+	public void run() {
+
+		while (true) {
+
+			synchronized (immortalsPopulation) {
+				while (paused) {
+					try {
 						immortalsPopulation.wait();
 					} catch (InterruptedException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-        		}
-        		immortalsPopulation.notifyAll();
-        	}
-            Immortal im;
+				}
+				immortalsPopulation.notifyAll();
 
-            int myIndex = immortalsPopulation.indexOf(this);
+				Immortal im;
 
-            int nextFighterIndex = r.nextInt(immortalsPopulation.size());
+				int myIndex = immortalsPopulation.indexOf(this);
 
-            //avoid self-fight
-            if (nextFighterIndex == myIndex) {
-                nextFighterIndex = ((nextFighterIndex + 1) % immortalsPopulation.size());
-            }
+				int nextFighterIndex = r.nextInt(immortalsPopulation.size());
 
-            im = immortalsPopulation.get(nextFighterIndex);
+				// avoid self-fight
+				if (nextFighterIndex == myIndex) {
+					nextFighterIndex = ((nextFighterIndex + 1) % immortalsPopulation.size());
+				}
 
-            this.fight(im);
+				im = immortalsPopulation.get(nextFighterIndex);
 
-            try {
-                Thread.sleep(1);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+				this.fight(im);
+				try {
+					Thread.sleep(1);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
 
-        }
+		}
 
-    }
+	}
 
-    public void fight(Immortal i2) {
+	public void fight(Immortal i2) {
 
-        if (i2.getHealth() > 0) {
-            i2.changeHealth(i2.getHealth() - defaultDamageValue);
-            this.health += defaultDamageValue;
-            updateCallback.processReport("Fight: " + this + " vs " + i2+"\n");
-        } else {
-            updateCallback.processReport(this + " says:" + i2 + " is already dead!\n");
-        }
+		if (i2.getHealth() > 0) {
+			i2.changeHealth(i2.getHealth() - defaultDamageValue);
+			this.health.addAndGet(defaultDamageValue);
+			updateCallback.processReport("Fight: " + this + " vs " + i2 + "\n");
+		} else {
+			updateCallback.processReport(this + " says:" + i2 + " is already dead!\n");
+		}
+	}
 
-    }
-    
-    public void pause() {
-    	paused=true;
-    }
-    
-    public void resumeImmortal() {
-    	paused=false;
-    }
+	public void pause() {
+		paused = true;
+	}
 
-    public void changeHealth(int v) {
-        health = v;
-    }
+	public void resumeImmortal() {
+		paused = false;
+	}
 
-    public int getHealth() {
-        return health;
-    }
+	public void changeHealth(int v) {
+		health.set(v);
+	}
 
-    @Override
-    public String toString() {
+	public int getHealth() {
+		return health.get();
+	}
 
-        return name + "[" + health + "]";
-    }
+	@Override
+	public String toString() {
+
+		return name + "[" + health + "]";
+	}
 
 }
